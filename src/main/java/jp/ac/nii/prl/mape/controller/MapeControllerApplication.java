b4d5c7f1dfd3813c1bf722f524b663c3bdb2dc10
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-11 10:13:29 +0900

    Generated Spring Web project

---
+++ src/main/java/jp/ac/nii/prl/mape/controller/MapeControllerApplication.java
@@ -0,0 +1,12 @@
+package jp.ac.nii.prl.mape.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapeControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapeControllerApplication.class, args);
+	}
+}
