0b6d8d2ebf00e603005755c65f6e09b8e2fa594b
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-11 12:15:50 +0900

    Empty controller

---
+++ src/main/java/jp/ac/nii/prl/mape/controller/controller/MapeControllerController.java
@@ -0,0 +1,13 @@
+package jp.ac.nii.prl.mape.controller.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Component
+@RequestMapping("/mape")
+public class MapeControllerController {
+
+	
+}
