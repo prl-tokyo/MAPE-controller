2e343066242e05cf6d67757510654fc628a4574e
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-19 01:28:04 +0900

    Skeleton method to trigger feedback loop

--- src/main/java/jp/ac/nii/prl/mape/controller/controller/MapeControllerController.java
+++ src/main/java/jp/ac/nii/prl/mape/controller/controller/MapeControllerController.java
@@ -1,13 +1,26 @@
 package jp.ac.nii.prl.mape.controller.controller;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @Component
 @RequestMapping("/mape")
 public class MapeControllerController {
 
+	@RequestMapping(value="/monitor", method=RequestMethod.GET)
+	public ResponseEntity<?> monitor(@RequestBody String monitor) {
+		
+		// create response
+		HttpHeaders httpHeaders = new HttpHeaders();
+		return new ResponseEntity<>(null, httpHeaders, HttpStatus.CREATED);
+	}
 	
 }
