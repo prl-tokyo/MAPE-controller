a3fc4d757999f412ce92f935552143023de78d1c
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-19 14:49:44 +0900

    AP model, for analysis and plan

---
+++ src/main/java/jp/ac/nii/prl/mape/controller/model/AP.java
@@ -0,0 +1,77 @@
+package jp.ac.nii.prl.mape.controller.model;
+
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class AP {
+	
+	@GeneratedValue
+	@Id
+	@JsonIgnore
+	private Long id;
+	
+	private String name;
+
+	private Collection<AP> predecessors;
+
+	private Analyser analyser;
+	
+	private Planner planner;
+	
+	private CombinedAP combinedAP;
+	
+	public Analyser getAnalyser() {
+		return analyser;
+	}
+	
+	public CombinedAP getCombinedAP() {
+		return combinedAP;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Planner getPlanner() {
+		return planner;
+	}
+
+	public Collection<AP> getPredecessors() {
+		return predecessors;
+	}
+
+	public void setAnalyser(Analyser analyser) {
+		this.analyser = analyser;
+	}
+
+	public void setCombinedAP(CombinedAP combinedAP) {
+		this.combinedAP = combinedAP;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPlanner(Planner planner) {
+		this.planner = planner;
+	}
+
+	public void setPredecessors(Collection<AP> predecessors) {
+		this.predecessors = predecessors;
+	}
+
+}
