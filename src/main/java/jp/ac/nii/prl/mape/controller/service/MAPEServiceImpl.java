a296e4f30cec873c63b4c6250eda21701aded1ff
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-19 09:59:58 +0900

    Skeleton MAPE service

---
+++ src/main/java/jp/ac/nii/prl/mape/controller/service/MAPEServiceImpl.java
@@ -0,0 +1,15 @@
+package jp.ac.nii.prl.mape.controller.service;
+
+import org.springframework.stereotype.Service;
+
+@Service("mapeService")
+public class MAPEServiceImpl implements MAPEService {
+
+	/* (non-Javadoc)
+	 * @see jp.ac.nii.prl.mape.controller.service.MAPEService#loop()
+	 */
+	@Override
+	public void loop() {
+		
+	}
+}
