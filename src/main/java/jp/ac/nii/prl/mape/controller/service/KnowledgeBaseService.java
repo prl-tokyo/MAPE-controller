bd9e0027956095c75a6f9c7409ff33d4896a1389
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-19 16:37:55 +0900

    Get and Put use KB webservice

--- src/main/java/jp/ac/nii/prl/mape/controller/service/KnowledgeBaseService.java
+++ src/main/java/jp/ac/nii/prl/mape/controller/service/KnowledgeBaseService.java
@@ -1,9 +1,11 @@
 package jp.ac.nii.prl.mape.controller.service;
 
+import jp.ac.nii.prl.mape.controller.model.KnowledgeBase;
+
 public interface KnowledgeBaseService {
 
-	void put(String bx, String view);
+	void put(KnowledgeBase kb, String bx, String view);
 
-	String get(String bx);
+	String get(KnowledgeBase kb, String bx);
 
 }
--- src/main/java/jp/ac/nii/prl/mape/controller/service/KnowledgeBaseServiceImpl.java
+++ src/main/java/jp/ac/nii/prl/mape/controller/service/KnowledgeBaseServiceImpl.java
@@ -1,6 +1,11 @@
 package jp.ac.nii.prl.mape.controller.service;
 
+import java.net.URI;
+
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import jp.ac.nii.prl.mape.controller.model.KnowledgeBase;
 
 @Service("knowledgeBaseService")
 public class KnowledgeBaseServiceImpl implements KnowledgeBaseService {
@@ -9,15 +14,19 @@ public class KnowledgeBaseServiceImpl implements KnowledgeBaseService {
 	 * @see jp.ac.nii.prl.mape.controller.service.KnowledgeBaseService#put(java.lang.String, java.lang.String)
 	 */
 	@Override
-	public void put(String bx, String view) {
-		
+	public void put(KnowledgeBase kb, String bx, String view) {
+		RestTemplate template = new RestTemplate();
+		URI location = template.postForLocation(kb.getBaseUrl() + "/bx", view);
+		view = template.getForObject(location, String.class);
 	}
 	
 	/* (non-Javadoc)
 	 * @see jp.ac.nii.prl.mape.controller.service.KnowledgeBaseService#get(java.lang.String)
 	 */
 	@Override
-	public String get(String bx) {
-		return null;
+	public String get(KnowledgeBase kb, String bx) {
+		RestTemplate template = new RestTemplate();
+		String view = template.getForObject(kb.getBaseUrl() + "/bx", String.class);
+		return view;
 	}
 }
