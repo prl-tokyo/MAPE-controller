3e7c3428408dc61862fe614c54edb282a5f9b4e7
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-19 16:43:44 +0900

    Execution calls execution webservice with execution view

--- src/main/java/jp/ac/nii/prl/mape/controller/service/ExecuterServiceImpl.java
+++ src/main/java/jp/ac/nii/prl/mape/controller/service/ExecuterServiceImpl.java
@@ -1,6 +1,8 @@
 package jp.ac.nii.prl.mape.controller.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import jp.ac.nii.prl.mape.controller.model.Executer;
 import jp.ac.nii.prl.mape.controller.model.KnowledgeBase;
@@ -8,11 +10,20 @@ import jp.ac.nii.prl.mape.controller.model.KnowledgeBase;
 @Service("executerService")
 public class ExecuterServiceImpl implements ExecuterService {
 
+	private final KnowledgeBaseService kbService;
+	
+	@Autowired
+	public ExecuterServiceImpl(KnowledgeBaseService kbService) {
+		this.kbService = kbService;
+	}
+	
 	/* (non-Javadoc)
 	 * @see jp.ac.nii.prl.mape.controller.service.ExecuterService#execute()
 	 */
 	@Override
 	public void execute(Executer executer, KnowledgeBase kb) {
-		
+		RestTemplate template = new RestTemplate();
+		String view = kbService.get(kb, "execution");
+		template.postForLocation(executer.getExecutionUrl(), view);
 	}
 }
