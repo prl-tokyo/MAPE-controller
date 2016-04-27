081dc7ab62058dd0d093179791ce0ee61146270a
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-27 11:51:52 +0900

    Rename Component to MAPEKComponent
    To avoid confusion with Spring's Component

---
+++ src/main/java/jp/ac/nii/prl/mape/controller/model/MAPEKComponent.java
@@ -0,0 +1,55 @@
+package jp.ac.nii.prl.mape.controller.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class MAPEKComponent {
+
+	@GeneratedValue
+	@Id
+	@JsonIgnore
+	private Long id;
+	
+	private String baseUrl;
+	
+	private String endpoint;
+	
+	private String parameter;
+
+	public String getBaseUrl() {
+		return baseUrl;
+	}
+
+	public String getEndpoint() {
+		return endpoint;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getParameter() {
+		return parameter;
+	}
+
+	public void setBaseUrl(String baseUrl) {
+		this.baseUrl = baseUrl;
+	}
+
+	public void setEndpoint(String endpoint) {
+		this.endpoint = endpoint;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setParameter(String parameter) {
+		this.parameter = parameter;
+	}
+
+}
