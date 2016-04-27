365e8326a7fda93d0361832f19745a35bd413824
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-27 18:27:04 +0900

    Enable scheduling

--- src/main/java/jp/ac/nii/prl/mape/controller/MapeControllerApplication.java
+++ src/main/java/jp/ac/nii/prl/mape/controller/MapeControllerApplication.java
@@ -2,8 +2,10 @@ package jp.ac.nii.prl.mape.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MapeControllerApplication {
 
 	public static void main(String[] args) {
