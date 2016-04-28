fe54c553ebadb515b1ea2784acc0ab9016f9fbec
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-28 15:27:30 +0900

    Get source from monitor and pass it on to knowledge base

--- src/main/java/jp/ac/nii/prl/mape/controller/service/MonitorServiceImpl.java
+++ src/main/java/jp/ac/nii/prl/mape/controller/service/MonitorServiceImpl.java
@@ -1,6 +1,10 @@
 package jp.ac.nii.prl.mape.controller.service;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import jp.ac.nii.prl.mape.controller.model.MAPEKComponent;
 import jp.ac.nii.prl.mape.controller.model.Timing;
@@ -18,7 +22,19 @@ public class MonitorServiceImpl implements MonitorService {
 
 	@Override
 	public void monitor(MAPEKComponent monitor, MAPEKComponent kb, Timing timing) {
-		// TODO Auto-generated method stub
+		RestTemplate restTemplate = new RestTemplate();
 		
+		// get the source from the monitor
+		String source = restTemplate.getForObject(monitor.getUrl() + "/monitor", String.class);
+		
+		System.out.println("Source fetched from monitor");
+		
+		// send source to the knowledge base
+		HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+		HttpEntity<String> entity = new HttpEntity<String>(source, headers);
+        restTemplate.postForEntity(kb.getUrl() + "/source", entity, String.class);
+        
+        System.out.println("Source sent to knowledge base");
 	}
 }
